6266d58b5acc1b1fa3a0ba6ebf06f5ebc422fa18
Thiago Marinho <tgmarinho@gmail.com>
2019-09-26 14:46:25 -0400

    React Native - Intro - Aula 09 - estilizando formulário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
-
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Octicons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="diff-added" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
